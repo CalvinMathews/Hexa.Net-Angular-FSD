2409ed88ba3d24cd34f3907f7b0ad47fc44a6da2
CalvinMathews <calvinriyadh@gmail.com>
2025-06-03 22:14:09 +0300

    C# code upload

---
+++ C# Case Study/CaseStudy8.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+namespace EmployeeManagementApp
+{
+
+    public class Employee
+    {
+        public int EmpCode { get; set; }
+        public string EmpName { get; set; }
+        public string Email { get; set; }
+        public string DeptName { get; set; }
+        public string Location { get; set; }
+    }
+
+    public interface IEmployee<T>
+    {
+        string PrintEmployeeDetails(T emp);
+    }
+
+
+    public interface IFullTimeEmployee : IEmployee<Employee>
+    {
+        double Basic { get; set; }
+        double Hra { get; set; }
+        double Da { get; set; }
+        double NetSalary { get; set; }
+
+        double CalculateSalary();
+    }
+
+
+    public interface IPartTimeEmployee : IEmployee<Employee>
+    {
+        double Basic { get; set; }
+        double Da { get; set; }
+        double NetSalary { get; set; }
+
+        double CalculateSalary();
+    }
+
+
+    public class FullTimeEmployee : Employee, IFullTimeEmployee
+    {
+        public double Basic { get; set; }
+        public double Hra { get; set; }
+        public double Da { get; set; }
+        public double NetSalary { get; set; }
+
+        public double CalculateSalary()
+        {
+            Hra = Basic * 0.15;
+            Da = Basic * 0.10;
+            NetSalary = Basic + Hra + Da;
+            return NetSalary;
+        }
+
+        public string PrintEmployeeDetails(Employee emp)
+        {
+            return $"{emp.EmpCode}\t{emp.EmpName}\t{emp.Email}\t{emp.DeptName}\t{emp.Location}";
+        }
+    }
+
+
+    public class PartTimeEmployee : Employee, IPartTimeEmployee
+    {
+        public double Basic { get; set; }
+        public double Da { get; set; }
+        public double NetSalary { get; set; }
+
+        public double CalculateSalary()
+        {
+            Da = Basic * 0.05;
+            NetSalary = Basic + Da;
+            return NetSalary;
+        }
+
+        public string PrintEmployeeDetails(Employee emp)
+        {
+            return $"{emp.EmpCode}\t{emp.EmpName}\t{emp.Email}\t{emp.DeptName}\t{emp.Location}";
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Part Time Employee
+            PartTimeEmployee pte = new PartTimeEmployee()
+            {
+                EmpCode = 100,
+                EmpName = "Scott",
+                Email = "scott@gmail.com",
+                DeptName = "ODC",
+                Location = "Pune",
+                Basic = 10000
+            };
+            pte.CalculateSalary();
+            Console.WriteLine(pte.PrintEmployeeDetails(pte));
+            Console.WriteLine("Net Salary For Part Time Employee is:" + pte.NetSalary);
+            Console.WriteLine();
+
+            // Full Time Employee
+            FullTimeEmployee fte = new FullTimeEmployee()
+            {
+                EmpCode = 101,
+                EmpName = "Tiger",
+                Email = "tiger@gmail.com",
+                DeptName = "Hr",
+                Location = "Pune",
+                Basic = 20000
+            };
+            fte.CalculateSalary();
+            Console.WriteLine(fte.PrintEmployeeDetails(fte));
+            Console.WriteLine("Net Salary For Full Time Employee is:" + fte.NetSalary);
+
+            Console.WriteLine("Press any key to continue . . .");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C# Case Study/StudentApp.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudentApp
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class StudentManager
+    {
+        private List<Student> list = new List<Student>();
+
+        public void Add(int id, string name)
+        {
+            list.Add(new Student { ID = id, Name = name });
+            Console.WriteLine("Student added");
+        }
+
+        public void ShowAll()
+        {
+            if (list.Count == 0)
+            {
+                Console.WriteLine("No student records");
+                return;
+            }
+
+            Console.WriteLine("Student List:");
+            foreach (Student s in list)
+            {
+                Console.WriteLine($"ID: {s.ID}, Name: {s.Name}");
+            }
+        }
+
+        public void Search(int id)
+        {
+            bool found = false;
+            foreach (Student s in list)
+            {
+                if (s.ID == id)
+                {
+                    Console.WriteLine($"Found: ID={s.ID}, Name={s.Name}");
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found)
+                Console.WriteLine("Student not found");
+        }
+
+        public void Remove(int id)
+        {
+            Student temp=null;
+            foreach (Student s in list)
+            {
+                if (s.ID == id)
+                {
+                    temp=s;
+                    break;
+                }
+            }
+
+            if (temp!= null)
+            {
+                list.Remove(temp);
+                Console.WriteLine("Student removed");
+            }
+            else
+            {
+                Console.WriteLine("Student not found");
+            }
+        }
+
+        public void Count()
+        {
+            Console.WriteLine($"Total students:  { list.Count}");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            StudentManager sm = new StudentManager();
+            sm.Add(1, "Ajay");
+            sm.Add(2, "Bob");
+            sm.Add(3, "Charlie");
+
+            while (true)
+            {
+                Console.WriteLine("\n----- Menu -----");
+                Console.WriteLine("1. Add Student");
+                Console.WriteLine("2. Show All");
+                Console.WriteLine("3. Search Student");
+                Console.WriteLine("4. Remove Student");
+                Console.WriteLine("5. Count Students");
+                Console.WriteLine("6. Exit");
+                Console.Write("Choose: ");
+
+                int ch = Convert.ToInt32(Console.ReadLine());
+
+                switch (ch)
+                {
+                    case 1:
+                        Console.Write("Enter ID: ");
+                        int id = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Enter Name: ");
+                        string name = Console.ReadLine();
+                        sm.Add(id, name);
+                        break;
+
+                    case 2:
+                        sm.ShowAll();
+                        break;
+
+                    case 3:
+                        Console.Write("Enter ID to search: ");
+                        int sid = Convert.ToInt32(Console.ReadLine());
+                        sm.Search(sid);
+                        break;
+
+                    case 4:
+                        Console.Write("Enter ID to remove: ");
+                        int rid = Convert.ToInt32(Console.ReadLine());
+                        sm.Remove(rid);
+                        break;
+
+                    case 5:
+                        sm.Count();
+                        break;
+
+                    case 6:
+                        Console.WriteLine("Goodbye!");
+                        return;
+
+                    default:
+                        Console.WriteLine("Invalid choice.");
+                        break;
+                }
+            }
+        }
+    }
+}
